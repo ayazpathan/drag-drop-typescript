4ad26d588d776b95b2cbbd02b3c0a9a22c4b3264
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-01-20 22:32:55 +0530

    Created active and finished project container div with TypeScript Classes

--- src/app.ts
+++ src/app.ts
@@ -48,6 +48,39 @@ function autobind(
     return adjDescriptor;
 }
 
+class Projectlist{
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished' ){
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent(){
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase()+ ' PROJECTS';
+  }
+
+  private attach(){
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+
 // Project Input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -149,3 +182,7 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activeProjectList = new Projectlist('active');
+const finishedProjectList = new Projectlist('finished');
+
+
