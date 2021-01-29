2b29869d1cd465b92ec3fd2e3329a67ee3d0504a
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-01-29 10:36:58 +0530

    Rendered proper project list with all details

--- src/app.ts
+++ src/app.ts
@@ -152,9 +152,40 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+  private project: Project;
+
+  get persons(){
+    if(this.project.people === 1){
+      return "1 person";
+    } 
+    else{
+      return `${this.project.people} persons`;
+    }
+  }
+
+  constructor(hostId: string, project: Project){
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+    
+    this.configure();
+    this.renderContent();
+  }
+
+  configure(){
+
+  }
+
+  renderContent(){
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
+
+  }
+}
 
 // Project List where all project will be listed based on what type of project it is
-
 class Projectlist extends Component<HTMLElement, HTMLDivElement>{
   assignedProjects: Project[];
 
@@ -190,9 +221,7 @@ class Projectlist extends Component<HTMLElement, HTMLDivElement>{
       const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
       listEl.innerHTML="";
       for(const projItem of this.assignedProjects){
-        const listItem = document.createElement("li");
-        listItem.textContent=projItem.title;
-        listEl?.appendChild(listItem);
+       new ProjectItem(this.element.querySelector("ul")!.id, projItem);
       }
   }
 
